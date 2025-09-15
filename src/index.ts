25f6f2ed5e0c2a7ea2a7b6be60c8f9fe1883b511
rzlmiooo <zrrizall10@example.com>
2025-09-15 08:32:01 +0700

    env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from 'elysia'
 import cors from '@elysiajs/cors'
 import swagger from '@elysiajs/swagger'
-
 import { auth } from './routes/auth'
 import { store } from './routes/stores'
 import { users } from './routes/users'
@@ -11,17 +10,14 @@ const app = new Elysia()
 app
   .use(cors())
   .use(swagger())
-
   .get('/', () => {
     return (
       "This is the default server route"
     )
   })
-
   .use(users)
   .use(store)
   .use(auth)
-
   .listen(5000)
 
 console.log('🦊 Elysia is running at http://localhost:5000');
